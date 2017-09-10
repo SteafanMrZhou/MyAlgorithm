64cced7cd622018bf4c8860f7567064c5772dbf9
Steafan Zhou <244007461@qq.com>
2017-09-10 11:20:54 +0800

    蓝桥杯-煤球数量

--- CoalballNumber.java
+++ CoalballNumber.java
@@ -8,12 +8,14 @@ public class CoalballNumber {
 	    int sum = 0;
 	    for(int i = 1; i <= 100; i++) {
 	    	perSum += i;
+// 		    获取每一层的数量
 	    	sum += perSum;
+// 		    获取总数量
 	    }
 	    System.out.println(sum);
 	}
 	
-
+ 
 	public static void main(String[] args) {
 		CoalballNumber cn = new CoalballNumber();
 		cn.doCoalballNumber();
